0edce50da76d35e38155029778ee2db4d4388d87
LuckyFish at macOS <2383690354@qq.com>
2026-06-06 07:26:12 +0800

    更新 CORS 中间件，添加对 HTTPS 前缀的支持并启用凭证

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,10 +10,13 @@ import (
 func CORSMiddleware(origin string) gin.HandlerFunc {
 	if origin == "" {
 		origin = "*"
+	} else if !strings.HasPrefix(origin, "http://") && !strings.HasPrefix(origin, "https://") {
+		origin = "https://" + origin
 	}
 
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Header("Access-Control-Max-Age", "86400")
